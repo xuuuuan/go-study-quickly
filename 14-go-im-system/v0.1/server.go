3f3bbd8226bcf7e6714840015a278bfdb827dd1d
xuan <zx786@vip.qq.com>
2023-06-16 14:40:41 +0800

    add: im-system 基础服务构建

---
+++ 14-go-im-system/v0.1/server.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"runtime"
+)
+
+type Server struct {
+	IP   string
+	Port int
+}
+
+func NewServer(ip string, port int) *Server {
+	server := Server{
+		IP:   ip,
+		Port: port,
+	}
+	return &server
+}
+
+func (server *Server) ConnHandler(conn net.Conn) {
+	// 处理当前连接业务
+	fmt.Println("connect success ", conn.LocalAddr())
+}
+
+func (server *Server) Start() {
+	// 1.socket listen
+	listener, err := net.Listen("tcp", fmt.Sprintf("%s:%d", server.IP, server.Port))
+	if err != nil {
+		fmt.Println("socket listen error:", err)
+		return
+	}
+	fmt.Println("start listening...")
+
+	// 4.socket close
+	defer func(l net.Listener) {
+		err := l.Close()
+		if err != nil {
+			fmt.Println("socket close error:", err)
+			runtime.Goexit()
+		}
+	}(listener)
+
+	for {
+		// 2.accept
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("listener accept error:", err)
+			continue
+		}
+		// 3.handle
+		server.ConnHandler(conn)
+	}
+}
