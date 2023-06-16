e25220b7a051f493686b858dfe505cd13ac8453f
xuan <zx786@vip.qq.com>
2023-06-16 16:58:15 +0800

    update: time

--- 01-hello-golang/hello.go
+++ 01-hello-golang/hello.go
@@ -9,4 +9,11 @@ import (
 func main() { // '{' 一定得和函数名一行 不然会编译报错
 	time.Sleep(1 * time.Second)
 	fmt.Println("Hello, World!")
+
+	currentTime := time.Now()
+	// 自定义输出格式
+	// time.DateTime = "2006-01-02 15:04:05"
+	formattedTime := currentTime.Format(time.DateTime)
+
+	fmt.Println("当前时间:", formattedTime)
 }
